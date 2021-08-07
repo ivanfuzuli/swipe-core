e0608aba164c3fa7ef85cf62ba8aff9a0a237ec9
Can Küçükyılmaz <can.kucukyilmaz@pusula.net.tr>
2021-08-07 16:25:26 +0300

    add password and username change

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.get("/", (req, res) => {
 
 app.use(LoginRoute);
 app.use(RegisterRoute);
-app.use(ProfileRoute);
+app.use("/profile", ProfileRoute);
 app.use("/forgot", ForgotRoute);
 app.use(VoteRoute);
 app.use(QuoteRoute);
