7258968c5d49a30f15fa4bc979db8a907c152928
Can Küçükyılmaz <can.kucukyilmaz@pusula.net.tr>
2021-08-31 00:32:29 +0300

    delete claps

--- routes/ProfileRoute.js
+++ routes/ProfileRoute.js
@@ -4,6 +4,7 @@ const passport = require("passport");
 const createError = require("http-errors");
 const User = require("../models/User");
 const Vote = require("../models/Vote");
+const Claps = require("../models/Claps");
 
 const Sentry = require("@sentry/node");
 
@@ -180,6 +181,7 @@ router.post(
     try {
       await User.deleteOne({ _id: user._id });
       await Vote.deleteMany({ _user_id: user._id });
+      await Claps.deleteMany({ _user_id: user._id });
     } catch (e) {
       Sentry.captureException(e);
       return next(createError(406, "User couldn't be deleted!"));
